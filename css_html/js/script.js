954d8f6a7825825d06f600d72f283e14356141b7
Thorgardd <rouviller.jeremy59@gmail.com>
2022-09-14 22:25:18 +0200

    Fin travail 15/09/2022

--- css_html/js/script.js
+++ css_html/js/script.js
@@ -1,5 +1,23 @@
+// Script JS pour vérifier
+// la fonctionnalité d'un bouton
 const btn = document.getElementById("btntest");
+if ( btn != undefined && btn != null ) {
+    btn.addEventListener("click", () => {
+        console.log("Bouton appuyé");
+    })
+}
 
-btn.addEventListener("click", () => {
-    console.log("Bouton appuyé");
-})
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const navPhone = document.getElementById("navigation-mobile")
+if (
+    hamburgerBtn != undefined &&
+    hamburgerBtn != null &&
+    navPhone != undefined &&
+    navPhone != null
+) {
+    hamburgerBtn.addEventListener("click", () => {
+        hamburgerBtn.classList.toggle("active");
+        navPhone.classList.toggle("active");
+        document.body.classList.toggle("nav-mobile-display")
+    });
+}
